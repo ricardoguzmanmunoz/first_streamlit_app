23bfd7ff38c6b6634cc281bbdff0582badd41a4b
Ricardo Guzmán <120434798+ricardoguzmanmunoz@users.noreply.github.com>
2022-12-22 02:13:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 
-# New Section to display fruitvice api response
+# New Section to display fruitvice api responsea
 streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
